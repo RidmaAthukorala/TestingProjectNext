5657a00b88b03751119269dccdf10f39021bfc3c
RidmaAthukorala <129721748+RidmaAthukorala@users.noreply.github.com>
2023-04-11 17:26:24 +0530

    index2 new

--- pages/components/index2.js
+++ pages/components/index2.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react'
 export default class index2 extends Component {
   render() {
     return (
-      <div>index2</div>
+        <div>
+            <div>index2</div>
+            <div>index2</div>
+            <div>index2</div>
+            <div>index2</div>
+            <div>index2</div>
+            <div>index2</div>
+            <div>index2</div>
+            <div>index2</div>
+        </div>
+      
     )
   }
 }
