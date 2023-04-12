01865737b3f943af98f30fc1777bf58a8854b28c
RidmaAthukorala <129721748+RidmaAthukorala@users.noreply.github.com>
2023-04-12 15:49:07 +0530

    fixes

--- pages/components/index2.js
+++ pages/components/index2.js
@@ -12,12 +12,15 @@ export default class index2 extends Component {
             <div>index2</div>
             <div>index2</div>
             <div>index2</div>
+<<<<<<< HEAD
             <div>index3</div>
             <div>index3</div>
             <div>index3</div>
             <div>index3</div>
             <div>index3</div>
             <div>index3</div>
+=======
+>>>>>>> 00b58f6 (index2 new)
         </div>
       
     )
