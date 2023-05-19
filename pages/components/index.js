a1e5736a817a6f9bfc3b8b0182cb3f1f407557c4
RidmaAthukorala <ridma@sonicdevs.com>
2023-05-19 11:54:36 +0530

    hgvcg

--- pages/components/index.js
+++ pages/components/index.js
@@ -3,10 +3,23 @@ import React, { Component } from "react";
 export default class index extends Component {
   render() {
     return (
+<<<<<<< Updated upstream
       <div>
         index
         <div>asdsssssa</div>
       </div>
     );
+=======
+<<<<<<< HEAD
+      <div>index</div>
+    )
+=======
+      <div>
+        index
+        <div>asdsa</div>
+      </div>
+    );
+>>>>>>> parent of a901769 (new)
+>>>>>>> Stashed changes
   }
 }
